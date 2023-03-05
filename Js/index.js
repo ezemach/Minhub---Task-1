e464ea8d8437aa971a8c94b3445516dfa1e73c36
Ezequiel Mazzucco <ezemazzucco@gmail.com>
2023-03-05 20:18:01 -0300

    tercer commit

--- Js/index.js
+++ Js/index.js
@@ -1,32 +1,43 @@
-const MyMain = document.getElementById( "main" );
+const MyMain = document.getElementById("main");
+
+const tarjeta = {
+      _id: "639c723b992482e5f2834be9",
+      name: "Collectivities Party",
+      image: "https://i.postimg.cc/Fs03hQDt/Collectivities-Party.jpg",
+      date: "2021-12-12",
+      description:
+        "Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
+      category: "Food Fair",
+      place: "Room A",
+      capacity: 45000,
+      assistance: 42756,
+      price: 5,
+      __v: 0,
+    };
 
 
-
-
-
-MyMain.innerHTML= template; 
-
-function crearTarjetaInner(data.events){
-    const template =
-  `<div class="card d-flex container-fluid b-brown3 text-light col-12 col-sm-6 col-md-6 col-lg-3 my-3" style="width: 15rem;">
-            <img src="${data.events.image}" class="card-img-top" alt="libros"  height="200px">
+function crearTarjetaInner(tarjeta){
+    let template =
+  `<div class="card d-flex container-fluid b-brown3 text-light col-12 col-sm-6 col-md-6 col-lg-3 my-3"      style="width: 15rem;">
+            <img src="${tarjeta.image}" class="card-img-top" alt="libros"  height="200px">
             <div class="card-body">
-                <h5 class="card-title">${data.events.name}</h5>
+                <h5 class="card-title">${tarjeta.name}</h5>
                 <div class="card-body d-flex justify-content-around">
-                    <p class ="card-text"> price: ${data.events.price}</p>
+                    <p class ="card-text"> price: ${tarjeta.price}</p>
                     <a href="./details.html" class="btn btn-warning">more details</a>
                 </div>
             </div>
-        </div>  
-`   
+    </div>  
+`
 return template
 }
 
-function pintarTarjetas( listaEventos, tarjeta ){
-    let template = ''
-    for( let data.events of listaEventos ){
-        template += crearTarjetaInner( data.events )
+function completarTarjetas(listaEventos, elemento ){
+    let template = '';
+    for( let tarjeta of listaEventos.events ){
+        template += crearTarjetaInner(tarjeta);
     }
-    tarjeta.innerHTML = template
+    elemento.innerHTML = template;
 }
 
+completarTarjetas(data,MyMain);
---
+++ Js/pastEvents.js
@@ -0,0 +1,57 @@
+const MyMainPast = document.getElementById("mainPast");
+
+const tarjeta = {
+      _id: "639c723b992482e5f2834be9",
+      name: "Collectivities Party",
+      image: "https://i.postimg.cc/Fs03hQDt/Collectivities-Party.jpg",
+      date: "2021-12-12",
+      description:
+        "Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
+      category: "Food Fair",
+      place: "Room A",
+      capacity: 45000,
+      assistance: 42756,
+      price: 5,
+      __v: 0,
+    };
+
+
+function crearTarjetaInner(tarjeta){
+    let template =
+  `<div class="card d-flex container-fluid b-brown3 text-light col-12 col-sm-6 col-md-6 col-lg-3 my-3"      style="width: 15rem;">
+            <img src="${tarjeta.image}" class="card-img-top" alt="libros"  height="200px">
+            <div class="card-body">
+                <h5 class="card-title">${tarjeta.name}</h5>
+                <div class="card-body d-flex justify-content-around">
+                    <p class ="card-text"> price: ${tarjeta.price}</p>
+                    <a href="./details.html" class="btn btn-warning">more details</a>
+                </div>
+            </div>
+    </div>  
+`
+return template
+}
+
+function completarTarjetas(listaEventos, elemento ){
+    let template = '';
+    for( let tarjeta of listaEventos){
+        template += crearTarjetaInner(tarjeta);
+    }
+    elemento.innerHTML = template;
+}
+
+function filtrarLista( lista, clave='date',valor='2021'){
+    let aux = []
+    for( let i = lista.events.length - 1 ; i >= 0 ; i-- ){
+        if( lista.events[i][clave].includes( valor )){
+            aux.push( lista.events[i] )
+        }
+    }
+    return aux
+}
+
+const tarjetaEventoPasado = filtrarLista(data , 'date', '2021' )
+
+completarTarjetas(tarjetaEventoPasado, MyMainPast)
+
+
